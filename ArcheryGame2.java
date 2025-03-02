b9c3424d52b6a36fb29414b50f62b27f3d853f7e
jhaytechsemicolon <mailjubilant@yahoo.com>
2025-03-02 16:01:58 +0100

    gdd

---
+++ ArcheryGame2.java
@@ -0,0 +1,101 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.security.SecureRandom;
+
+public class ArcheryGame2{
+
+static SecureRandom random = new SecureRandom();
+	public static int[][] archeryDataCollection(int [][]  results){
+		
+		
+		for (int count = 0; count < results.length; count++){
+			for(int counter = 0; counter < results[count].length; counter++ ){
+			int points = random.nextInt(10);
+			results[count][counter] = points;
+  }
+}
+return results;
+}
+
+	public static int[] totalForPlayer(int [][] results){
+		int [] totals  = new int [results.length];
+		for (int rows = 0; rows < results.length ; rows++){
+		int total = 0;
+
+			for(int column = 0; column < results[rows].length; column++){
+			total += results[rows][column];
+			
+		}
+totals[rows] = total;
+
+	}
+return totals;
+
+	
+	}
+
+public static int playerWon(int [] total){
+		int index = 0;
+		int largest = total[0];
+		for(int count = 0; count < total.length; count++){
+			if (total[count] > largest){
+			largest = total[count];
+		  index = count;
+	}
+
+
+}
+
+return index;
+}
+
+
+
+public static void main (String...args){
+	
+	  int [][] archeryData = new int [4][3];
+    int [][]result  = archeryDataCollection(archeryData);
+    int [] total = totalForPlayer(result);
+	 int winner = playerWon(total);
+
+
+ 
+	for(int rows = 0; rows < result.length; rows++  ){
+	System.out.print("player   " + (rows + 1)+ "	");
+		for(int column = 0; column < result[rows].length; column++ ){
+
+System.out.print(result[rows][column] + "	");
+}
+System.out.println(total[rows]);
+
+}
+
+System.out.println((winner + 1));
+
+
+
+	 }
+}
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
